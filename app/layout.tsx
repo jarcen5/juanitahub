bf391ab92b85459a6440e4761162f011d8cdfc4f
Junie Arcene <juniearcene@outlook.com>
2026-09-14 13:28:47 -0400

    Load redesign styles on preview branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import SiteNavigation from '@/components/SiteNavigation'
 import './globals.css'
+import './redesign.css'
 
 export const metadata: Metadata = {
   title: 'Juanita Hub',
